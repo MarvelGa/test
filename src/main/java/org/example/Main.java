10eb4f7a01081fa6015603fc7eeb900ae73362a0
Halyna Tesliuk <halyna_tesliuk@epam.com>
2024-04-01 10:57:17 +0200

    deleted files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello task2");
-        System.out.println("Test1");
         System.out.println("Hello world!");
     }
 }
