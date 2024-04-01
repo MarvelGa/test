4328872886716fb693ed53550ac5dfa727ba2cd0
Halyna Tesliuk <halyna_tesliuk@epam.com>
2024-04-01 12:32:29 +0200

    feat(TEST-4): modified file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Test1");
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world4!");
+        System.out.println("Hello world!");
     }
 }
